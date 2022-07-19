e294655612da2f7c722913be76d4ecb06cd4345f
Dany <mrushidany@gmail.com>
2022-07-19 18:20:54 +0300

    Changed the string type column to float

--- database/migrations/2022_07_19_150614_create_years_table.php
+++ database/migrations/2022_07_19_150614_create_years_table.php
@@ -15,6 +15,37 @@ return new class extends Migration
     {
         Schema::create('years', function (Blueprint $table) {
             $table->id();
+            $table->float('1990');
+            $table->float('1991');
+            $table->float('1992');
+            $table->float('1993');
+            $table->float('1994');
+            $table->float('1995');
+            $table->float('1996');
+            $table->float('1997');
+            $table->float('1998');
+            $table->float('1999');
+            $table->float('2000');
+            $table->float('2001');
+            $table->float('2002');
+            $table->float('2003');
+            $table->float('2004');
+            $table->float('2005');
+            $table->float('2006');
+            $table->float('2007');
+            $table->float('2008');
+            $table->float('2009');
+            $table->float('2010');
+            $table->float('2011');
+            $table->float('2012');
+            $table->float('2013');
+            $table->float('2014');
+            $table->float('2015');
+            $table->float('2016');
+            $table->float('2017');
+            $table->float('2018');
+            $table->float('2019');
+            $table->float('2020');
             $table->timestamps();
         });
     }
